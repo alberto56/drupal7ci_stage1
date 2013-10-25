9e336bd7b5b69780ffa4761cff4645e9360fccba
Albert Albala <albert@mediatribe.net>
2013-10-24 22:20:16 -0400

    consolidate commands

--- sites/all/modules/custom/demo_deploy/ci-scripts/deploy.sh
+++ sites/all/modules/custom/demo_deploy/ci-scripts/deploy.sh
@@ -1,8 +1,8 @@
 # sample usage:
 # sites/all/modules/custom/demo_deploy/ci-scripts/deploy.sh tp1@server.example.com /Applications/MAMP/htdocs/drupalcamp-prod-d7 prod
 
-ssh $1 "cd $2 && git pull origin $3"
-ssh $1 "drush -r $2 rr &&
+ssh $1 "cd $2 && git pull origin $3 &&
+drush -r $2 rr &&
 drush -r $2 vset maintenance_mode 1 &&
 drush -r $2 cc all &&
 drush -r $2 en demo_deploy -y &&
