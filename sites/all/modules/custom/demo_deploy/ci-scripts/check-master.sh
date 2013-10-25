d353a2a7426cfdf5fd75e889e58701245c2e9eee
Albert Albala <albert@mediatribe.net>
2013-10-24 22:04:31 -0400

    executable shell

--- sites/all/modules/custom/demo_deploy/ci-scripts/check-master.sh
+++ sites/all/modules/custom/demo_deploy/ci-scripts/check-master.sh
@@ -1,5 +1,7 @@
 # see https://github.com/drush-ops/drush/issues/212 for the use of grep -v 
 
+# this won't work unless you set your base_url in sites/default/settings.php
+
 MYTESTGROUP=demo
 RESULT=$(drush test-run $MYTESTGROUP 2>&1)
 echo $RESULT
