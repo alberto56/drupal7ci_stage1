5ea2413425a045520c0565af62a6cab934bb5694
Albert Albala <albert@mediatribe.net>
2013-10-24 22:10:13 -0400

    added enabling the module to the deployment procedure

--- sites/all/modules/custom/demo_deploy/ci-scripts/deploy.sh
+++ sites/all/modules/custom/demo_deploy/ci-scripts/deploy.sh
@@ -2,9 +2,14 @@
 # sites/all/modules/custom/demo_deploy/ci-scripts/deploy.sh tp1@server.example.com /Applications/MAMP/htdocs/drupalcamp-prod-d7 prod
 
 ssh $1 "cd $2 && git pull origin $3"
-ssh $1 "drush -r $2 vset maintenance_mode 1"
-ssh $1 "drush -r $2 rr"
-ssh $1 "drush -r $2 updb -y"
-ssh $1 "drush -r $2 cc all"
-ssh $1 "drush -r $2 cron"
-ssh $1 "drush -r $2 vset maintenance_mode 0"
+ssh $1 "drush -r $2 rr &&
+drush -r $2 vset maintenance_mode 1 &&
+drush -r $2 cc all &&
+drush -r $2 en demo_deploy -y &&
+drush -r $2 updb -y &&
+drush -r $2 cc all &&
+drush -r $2 cron &&
+drush -r $2 vset maintenance_mode 0"
+
+# todo: backup the db here with something like
+# drush -r $2 sql-dump > ~/mysql-$(git log -n1 --pretty='%c').sql
